5fb81f75ee383304b55a310683f0eae5bf42989c
Veronika <veronika.apakova@mail.ru>
2023-04-28 21:15:17 +0300

    add tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,19 @@
 import unittest
-from utils import arrs
+from unittest_proj.utils.arrs import get,my_slice
 
 
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(get([1, 2, 3], -1, 'test'), 'test')
+        self.assertEqual(get([1, 2, 3], 0, 'test'), 1)
 
     def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(my_slice([], 1, 2), [])
+        self.assertEqual(my_slice([1, 2, 3], -1, -3), [])
+        self.assertEqual(my_slice([1, 2, 3], -1), [3])
+        self.assertEqual(my_slice([1, 2, 3], -2), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], -10), [1, 2, 3])
